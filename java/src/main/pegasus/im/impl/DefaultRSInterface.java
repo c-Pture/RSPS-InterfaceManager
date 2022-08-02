205b63e9410aefce6979897fa25cd6a52ed004e8
cPture <knakowskimanuel@live.de>
2022-08-03 00:10:48 +0200

    Update DefaultRSInterface.java

--- java/src/main/pegasus/im/impl/DefaultRSInterface.java
+++ java/src/main/pegasus/im/impl/DefaultRSInterface.java
@@ -1,3 +1,20 @@
+/**
+	c-pture/Pegasus (Manu on Rune-Server)
+	Copyright (c) 2022 - Pegasus/C-Pture Team
+	Written by Manuel K. - Germany
+	Version: 1.1.0-beta.1
+	
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package main.pegasus.im.impl;
 
 import main.pegasus.im.RSAbstractInterface;
